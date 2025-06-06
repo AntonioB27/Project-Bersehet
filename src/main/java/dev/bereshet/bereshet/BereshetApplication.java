2fdb9b964342477e2537f2be2b12b6db12758ff0
Antonio Becic <becicantonio27@gmail.com>
2025-06-06 09:03:38 +0200

    Setup

---
+++ src/main/java/dev/bereshet/bereshet/BereshetApplication.java
@@ -0,0 +1,13 @@
+package dev.bereshet.bereshet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BereshetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BereshetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bereshet/bereshet/BereshetApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bereshet.bereshet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BereshetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
